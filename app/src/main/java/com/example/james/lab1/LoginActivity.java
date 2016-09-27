d49c6c7d3f28433755a37f86860dd26dae2feb4f
james <jamesjin37@gmail.com>
2016-09-27 16:08:56 -0400

    lab 3 finished

--- app/src/main/java/com/example/james/lab1/LoginActivity.java
+++ app/src/main/java/com/example/james/lab1/LoginActivity.java
@@ -1,13 +1,24 @@
 package com.example.james.lab1;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+
+    private Button loginButton;
+    private static final String PREFERENCE_FILE = "MyPrefsFile";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,62 @@ public class LoginActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences(PREFERENCE_FILE,Context.MODE_PRIVATE).edit();
+                EditText loginName =  (EditText)findViewById(R.id.loginNameEditText);
+                editor.putString("DefaultEmail", loginName.getText().toString());
+                editor.commit();
+
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(ACTIVITY_NAME, "In onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(ACTIVITY_NAME, "In onStart()");
+        super.onStart();
+
+        String defaultEmail;
+        SharedPreferences prefs = getSharedPreferences(PREFERENCE_FILE,Context.MODE_PRIVATE);
+        defaultEmail = prefs.getString("DefaultEmail", "email@domain.com");
+
+        EditText loginName =  (EditText)findViewById(R.id.loginNameEditText);
+        loginName.setText(defaultEmail);
+
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(ACTIVITY_NAME, "In onPause()");
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(ACTIVITY_NAME, "In onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+        super.onDestroy();
     }
 
 }
